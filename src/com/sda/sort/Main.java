8932a83fae76e0491c2e5331d22c32a4b9dcd739
Wik81 <wik99@wp.pl>
2018-10-21 15:52:19 +0200

    zmiana w main

--- src/com/sda/sort/Main.java
+++ src/com/sda/sort/Main.java
@@ -3,6 +3,25 @@ package com.sda.sort;
 public class Main {
 
     public static void main(String[] args) {
-	
+
+        int tab[] = {1, 33, 32, 5, 12, 45, 342, -3, 32};
+
+        for (int i = 0; i <tab.length; i++){
+            System.out.println(tab[i]);
+        }
+
+        for (int i = 0; i < tab.length - 1; i++){
+            for (int j = 0; j < tab.length - 1; j ++){
+
+                if(tab[j] > tab[j + 1]){
+                    int tmp = tab[j];
+                    tab[j] = tab[j + 1];
+                    tab[j + 1] = tmp;
+                }
+            }
+        }
+        for (int i = 0; i < tab.length; i++){
+            System.out.println(tab[i]);
+        }
     }
 }
